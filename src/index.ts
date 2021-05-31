5c8a757fe49410b2865e308d5fb8b22f5b488052
PD Jell <>
2021-05-31 17:36:20 +0900

    add comment

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
 import { ApolloServer, gql } from 'apollo-server'
 
+/**
+ * typeDef(s)
+ * - GraphQL Schema를 정의하는 곳
+ * Object, Query, Mutation, Input이 있다.
+ * gql과 Tagged Template Literals로 작성한다.
+ */
 const typeDefs = gql`
     type Query {
         "A simple type for getting started!"
         hello: String
     }
-`
+` // graphQL에서 쓰는 스키마를 정의함. 어떤식으로 요청할건지....여기서 hello 라는 string을 정의한다.
 
+/**
+ * resolver(s)
+ * - Schema에 해당하는 구현을 하는 곳
+ * 요청을 받아 데이터를 조회, 수정, 삭제함
+ */
 const resolvers = {
   Query: {
     hello: () => 'world'
   }
-}
+} // 스키마에 해당하는 구현체를 적음
 
 const server = new ApolloServer({
   typeDefs,
